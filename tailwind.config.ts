e20a9c4d10b775d4afeb46e66531910ac92bf81a
Ezzdin Atef <ezzdin1125@gmail.com>
2024-10-03 12:19:58 +0300

    update color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "primary-1": "var(--primary-1)",
+        "primary-2": "var(--primary-2)",
+        "primary-3": "var(--primary-3)",
+
+        "black-1": "var(--black-1)",
+        "black-2": "var(--black-2)",
+        "black-3": "var(--black-3)",
+
+        "gray-1": "var(--gray-1)",
+        "gray-2": "var(--gray-2)",
+        "gray-3": "var(--gray-3)",
+
+        "light-gray-1": "var(--light-gray-1)",
+        "light-gray-2": "var(--light-gray-2)",
+        "light-gray-3": "var(--light-gray-3)",
+
+        "red-1": "var(--red-1)",
       },
     },
   },
